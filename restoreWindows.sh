01901830108ab38db1286853a6b227a29eafed20
JeffBrin <bringolfj@hotmail.com>
2024-09-15 07:17:33 -0400

    MacOS Entirely Complete

--- restoreWindows.sh
+++ restoreWindows.sh
@@ -1,4 +1,1 @@
-osascript -e 'tell application "Google Chrome" to activate
-tell application "System Events"
-    keystroke "n" using {command down}
-end tell'
+osascript -e 'tell application "Google Chrome" to activate'
